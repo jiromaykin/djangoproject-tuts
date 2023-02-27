59ecb013ace88b618d548e398e0e6289c057221c
Jiro Ghianni <jiro@maykinmedia.nl>
2023-02-27 19:15:20 +0100

    :lipstick: added choices to 1 question + CSS styles

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # tell the admin that Question objects have an admin interface
 from .models import Question
+from .models import Choice
 
 admin.site.register(Question)
+admin.site.register(Choice)
